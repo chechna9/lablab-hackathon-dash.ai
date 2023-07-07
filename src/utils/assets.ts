2ba3e3455af7075b70f356e3d6775dd25fb4af3e
chechna9 <ja_bouchouareb@esi.dz>
2023-07-07 10:51:02 +0100

    added assets and setup tailwind css

---
+++ src/utils/assets.ts
@@ -0,0 +1,13 @@
+import whiteLogo from '../assets/whiteLogo.svg';
+import blackLogo from '../assets/blackLogo.svg';
+import shape1 from '../assets/shape1.svg';
+import shape2 from '../assets/shape2.svg';
+import shape3 from '../assets/shape3.svg';
+
+export const assets = {
+    whiteLogo,
+    blackLogo,
+    shape1,
+    shape2,
+    shape3,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        myDark: "#19647E",
+        myDarkBlue: "#19647E",
+        myGreen: "#28CDBA",
+        myRed: "#FF554D",
+        myPurple: "#96AAFE",
+      },
+     
+
+      
+    },
+  },
+  plugins: [],
+};
