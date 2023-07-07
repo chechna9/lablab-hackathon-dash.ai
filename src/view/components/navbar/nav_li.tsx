5bce2f53cedcc63bc606e3a56b5dac3df37b1372
chechna9 <ja_bouchouareb@esi.dz>
2023-07-07 12:40:22 +0100

    added is active status

--- src/view/components/navbar/nav_li.tsx
+++ src/view/components/navbar/nav_li.tsx
@@ -1,9 +1,16 @@
 interface NavLiProps {
   children: string;
+  isActive: boolean;
+  onClick: () => void;
 }
 const NavLi = (props: NavLiProps) => {
   return (
-    <li className="hover:font-semibold cursor-pointer transition-all">
+    <li
+      className={`hover:font-semibold cursor-pointer transition-all ${
+        props.isActive ? "font-semibold" : ""
+      }`}
+      onClick={props.onClick}
+    >
       {props.children}
     </li>
   );
--- src/view/components/navbar/navbar.tsx
+++ src/view/components/navbar/navbar.tsx
@@ -1,19 +1,52 @@
+import { useState } from "react";
 import { assets } from "../../../utils/assets";
 
 import NavLi from "./nav_li";
 
+
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+  const handleActiveLink = (link: string) => {
+    setActiveLink(link);
+    console.log(activeLink === "home");
+    
+  };
   return (
     <nav className="flex justify-evenly items-center my-2">
-      <img src={assets.blackLogo} alt="" className="w-[80px]"/>
+      <img src={assets.blackLogo} alt="" className="w-[80px]" />
       <ul className="flex justify-evenly w-[50vw] ">
-        <NavLi>Home</NavLi>
-        <NavLi>What We Do</NavLi>
-        <NavLi>Service</NavLi>
-        <NavLi>Projet</NavLi>
-        <NavLi>Contact</NavLi>
+        <NavLi
+          onClick={() => handleActiveLink("home")}
+          isActive={activeLink === "home"}
+        >
+          Home
+        </NavLi>
+        <NavLi
+          onClick={() => handleActiveLink("what-we-do")}
+          isActive={activeLink === "what-we-do"}
+        >
+          What We Do
+        </NavLi>
+        <NavLi
+          onClick={() => handleActiveLink("service")}
+          isActive={activeLink === "service"}
+        >
+          Service
+        </NavLi>
+        <NavLi
+          onClick={() => handleActiveLink("project")}
+          isActive={activeLink === "project"}
+        >
+          Projet
+        </NavLi>
+        <NavLi
+          onClick={() => handleActiveLink("contact")}
+          isActive={activeLink === "contact"}
+        >
+          Contact
+        </NavLi>
       </ul>
-      <button className="bg-myPurple hover:bg-myDarkBlue py-3 px-8 text-white rounded-full font-medium transition-all">
+      <button className="bg-myPurple hover:bg-myDarkBlue py-3 px-10 text-white rounded-full font-medium transition-all">
         Login
       </button>
     </nav>
