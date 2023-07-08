2f648cc74612b380fa8e373ac500a17ab76b5871
chechna9 <ja_bouchouareb@esi.dz>
2023-07-08 06:08:42 +0100

    scrol always on bottom

--- src/view/components/message_card.tsx
+++ src/view/components/message_card.tsx
@@ -1,5 +1,5 @@
 import { MessageInterface } from "../../interfaces/message_interface";
-import { assets } from "../../utils/assets";
+// import { assets } from "../../utils/assets";
 
 const MessageCard = (props: MessageInterface) => {
   return (
@@ -9,11 +9,11 @@ const MessageCard = (props: MessageInterface) => {
       } text-white text-[18px] font-normal p-2 rounded-2xl max-w-[25vw] break-words relative`}
     >
       {props.message}
-      {props.fromBot && (
+      {/* {props.fromBot && (
         <div className="absolute bg-white rounded-[120%] left-0 -translate-x-[100%] top-0 -translate-y-[50%] w-14 h-14 flex items-center justify-center">
           <img src={assets.aiIcon} alt="" className="w-10" />
         </div>
-      )}
+      )} */}
     </p>
   );
 };
--- src/view/screens/chat bot/chatbot_screen.tsx
+++ src/view/screens/chat bot/chatbot_screen.tsx
@@ -5,11 +5,11 @@ import SideBar from "./side_bar";
 import {
   faChevronDown,
   faChevronLeft,
- 
 } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 import { DisscussionInterface } from "../../../interfaces/disscussion_interface";
 import ChatMain from "./chat_main";
+import { useEffect, useRef, useState } from "react";
 
 const ChatBotScreen = () => {
   const sidebarProps: SideBarInterface = {
@@ -74,29 +74,54 @@ const ChatBotScreen = () => {
       },
     ],
   };
-  const disscussion:DisscussionInterface={
-    messages:[
+  const [disscussion, setDisscussion] = useState<DisscussionInterface>({
+    messages: [
       {
-        message:"Hello",
-        fromBot:false,
+        message: "Hello",
+        fromBot: false,
       },
       {
-        message:"Hello am Dash Ai Bot",
-        fromBot:true,
+        message: "Hello am Dash Ai Bot",
+        fromBot: true,
       },
       {
-        message:"We win ?",
-        fromBot:false
+        message: "We win ?",
+        fromBot: false,
       },
       {
-        message:"SIuuuuuu 3-0 Lorem ipsum dolor sit amet consectetur. Vulputate pellentesque suspendisse eget vitae turpis velit proin nibh dolor. Sit at non lorem aliquam iaculis nisi ullamcorper. Sollicitudin duis et scelerisque et consectetur. Ultricies laoreet quam orci nunc sed. Urna purus vestibulum viverra suscipit non a. Tempor euismod nullam lectus ullamcorper non nunc sit fringilla dignissim. Lobortis ullamcorper ut nulla lacus consectetur neque. Felis lobortis enim suspendisse ut sed massa neque sit. Eu eu sit nulla sit leo pellentesque urna sit. Ultrices tristique mi imperdiet pellentesque tempus sapien purus adipiscing turpis. Nibh aliquam nunc tempor ac. Nunc aliquet ipsum velit augue risus. Orci mauris amet bibendum facilisis eget at lorem. Duis at arcu ipsum diam lobortis amet.",
-        fromBot:true,
+        message:
+          "SIuuuuuu 3-0 Lorem ipsum dolor sit amet consectetur. Vulputate pellentesque suspendisse eget vitae turpis velit proin nibh dolor. Sit at non lorem aliquam iaculis nisi ullamcorper. Sollicitudin duis et scelerisque et consectetur. Ultricies laoreet quam orci nunc sed. Urna purus vestibulum viverra suscipit non a. Tempor euismod nullam lectus ullamcorper non nunc sit fringilla dignissim. Lobortis ullamcorper ut nulla lacus consectetur neque. Felis lobortis enim suspendisse ut sed massa neque sit. Eu eu sit nulla sit leo pellentesque urna sit. Ultrices tristique mi imperdiet pellentesque tempus sapien purus adipiscing turpis. Nibh aliquam nunc tempor ac. Nunc aliquet ipsum velit augue risus. Orci mauris amet bibendum facilisis eget at lorem. Duis at arcu ipsum diam lobortis amet.",
+        fromBot: true,
       },
-    ]
-
+    ],
+  });
+  const addMessage = (message: string) => {
+    setDisscussion({
+      messages: [
+        ...disscussion.messages,
+        {
+          message,
+          fromBot: false,
+        },
+      ],
+    });
   };
+
+  const handleSendMessage = () => {
+    if (inputRef.current?.value) {
+      addMessage(inputRef.current.value);
+      inputRef.current.value = "";
+      
+    }
+  };
+  useEffect(() => {
+    if (chatRef)
+      chatRef!.current!.scrollTop = chatRef!.current!.scrollHeight;
+  }, [disscussion]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const chatRef = useRef<HTMLDivElement | null>(null);
   return (
-    <main className="h-screen relative flex overflow-clip">
+    <main className="h-screen relative flex ">
       {/* side bar */}
       <section className="flex-1 py-3">
         <div className="flex items-center justify-around">
@@ -144,11 +169,10 @@ const ChatBotScreen = () => {
           />
         </div>
         {/* chat */}
-        <div className="mx-[5vw] flex flex-col flex-auto ">
+        <div className="mx-[5vw] flex flex-col flex-auto overflow-y-auto  space-y-1 mt-2">
           {/* chatt */}
-          <div className="flex-auto">
-           <ChatMain {...disscussion}/>
-            
+          <div ref={chatRef} className="flex-auto overflow-y-auto ">
+            <ChatMain {...disscussion} />
           </div>
           {/* chatt input */}
           <div className="bg-myPurple flex p-3 rounded-2xl items-center">
@@ -158,11 +182,15 @@ const ChatBotScreen = () => {
               className="cursor-pointer"
             />
             <input
+              ref={inputRef}
               type="text"
               className="bg-myPurple outline-none caret-white text-white w-full mx-6 placeholder:text-white"
               placeholder="Ask any question about the selected product reviews"
             />
-            <motion.button whileHover={{ rotate: [0, -10, 10, 0] }}>
+            <motion.button
+              whileHover={{ rotate: [0, -10, 10, 0] }}
+              onClick={handleSendMessage}
+            >
               <img src={assets.sendIcon} alt="" />
             </motion.button>
           </div>
